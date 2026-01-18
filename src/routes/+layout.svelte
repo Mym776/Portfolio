8bb6b5e8508756e6910152c6e47c8fe3d76648e1
Mym776 <Mymhs7@gmail.com>
2026-01-18 22:51:10 +0300

    trials

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 	import Name from './App.svelte';
+	import Tile from './tile.svelte'
 	let { children } = $props();
+
+	let st = $state(["biking","golfing","swimming"])
+
+	function s (){
+		let temp = st.shift()
+        st.push(temp)
+		
+	}
+	setInterval(s, 3000);
+
 </script>
 
 <svelte:head>
@@ -10,3 +21,16 @@
 
 
 <Name />
+<div>
+	<Tile hobby={st[0]}/>
+	<Tile hobby={st[1]}/>
+	<Tile hobby={st[2]}/>	
+</div>
+
+<style>
+	div{
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+	}
+</style>
---
+++ src/routes/tile.svelte
@@ -0,0 +1,31 @@
+<script>
+    let {hobby} = $props()
+    
+</script>
+
+<div>
+    <h3>{hobby}</h3>
+    <p>
+        i like this {hobby} because, i like this hobby and this hobby is imprtant because this hobby is important 
+    </p>
+</div>
+
+<style>
+    div {
+        padding: 1%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        background-color: grey;
+        align-items: center;
+        width: 15%;
+        border-radius: 4%;
+    }
+    h3 {
+        font-family: monospace;
+
+    }
+    p {
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
